fe31303d56a719a16252956bb7910955979f6561
Mercedes Quiñajo <iqcmercedes@gmail.com>
2015-08-02 17:07:32 -0400

    penfigo ampollas

--- app/Controller/PenfigoampollasController.php
+++ app/Controller/PenfigoampollasController.php
@@ -4,19 +4,29 @@ class PenfigoampollasController extends AppController {
 
     public $uses = array('Penfigo', 'Area', 'Tipoampolla', 'Penfigoampolla');
 
-    public function index() {
-     $pampollas = $this->Penfigoampolla->find ('all');
-     $this->set(compact('pampollas'));
+  public function penfigoampolla($idPenfigo = null) {
+    $this->layout = 'ajax';
+    $arampollas = $this->Area->find('list', array('fields' => 'Area.nombre'));
+    $pampolla = $this->Penfigoampolla->find('all', array(
+      'recursive' => 0,
+      'conditions' => array('Penfigoampolla.penfigo_id' => $idPenfigo)
+    ));
+    $penfigo = $this->Penfigo->findByid($idPenfigo, null, null, -1);
+    $this->set(compact('pampolla', 'arampollas', 'penfigo', 'idPenfigo'));
+  }
+  
+    public function registra($idPenfigo = null) {
+    if(!empty($this->request->data['Penfigoampolla']['area_id'])){
+      $encuentra = $this->Penfigoampolla->find('first',array(
+        'recursive' => -1,
+        'conditions' => array('area_id' => $this->request->data['Penfigoampolla']['area_id'],'penfigo_id' => $idPenfigo)
+      ));
+      if(!empty($encuentra)){
+        $this->request->data['Penfigoampolla']['id'] = $encuentra['Penfigoampolla']['id'];
+      }
+      $this->Penfigoampolla->create();
+      $this->Penfigoampolla->save($this->request->data['Penfigoampolla']);
     }
-
-    public function penfigoampolla ($idPampolla = null){
-        $this->layout = 'ajax';
-        if (!empty($this->request->data)){
-            
-        }
-        $areas = $this->Area->find('list', array('fields' => 'nombre'));
-        $penfigo=$this->Penfigo->find('list', array('fields'=>'nombre'));
-        $ampolla=  $this->Tipoampolla->find('list', array('fields'=>'nombre'));
-        $this->set(compact('areas','penfigo','ampolla'));
-    }
+    exit;
+  }
 }
