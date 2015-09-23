9eca17db9de4f35b4a1f49264a6bcc6cb09fab94
eynar <eynar@labware.com.bo>
2015-09-23 13:33:09 -0400

    cambios

--- app/Controller/PenfigoampollasController.php
+++ app/Controller/PenfigoampollasController.php
@@ -3,12 +3,15 @@
 class PenfigoampollasController extends AppController {
 
   public $layout = 'penfigo';
-    public $uses = array('Penfigo', 'Area', 'Tipoampolla', 'Penfigoampolla');
+  public $uses = array('Penfigo', 'Area', 'Tipoampolla', 'Penfigoampolla');
 
   public function penfigoampolla($idPenfigo = null) {
     $this->layout = 'ajax';
     $arampollas = $this->Area->find('list', array('fields' => 'Area.nombre'));
-    $tampolla=  $this->Tipoampolla->find('list',array('fields'=>'Tipoampolla.nombre'));
+    $this->Tipoampolla->virtualFields = array(
+      'nombre_completo' => "CONCAT(Tipoampolla.nombre,' (',Tipoampolla.tipo,')')"
+    );
+    $tampolla = $this->Tipoampolla->find('list', array('fields' => 'Tipoampolla.nombre_completo'));
     $pampolla = $this->Penfigoampolla->find('all', array(
       'recursive' => 0,
       'conditions' => array('Penfigoampolla.penfigo_id' => $idPenfigo)
@@ -16,14 +19,18 @@ class PenfigoampollasController extends AppController {
     $penfigo = $this->Penfigo->findByid($idPenfigo, null, null, -1);
     $this->set(compact('pampolla', 'arampollas', 'penfigo', 'idPenfigo', 'tampolla'));
   }
-  
-    public function registra($idPenfigo = null) {
-    if(!empty($this->request->data['Penfigoampolla']['area_id'])){
-      $encuentra = $this->Penfigoampolla->find('first',array(
+
+  public function registra($idPenfigo = null) {
+    if (!empty($this->request->data['Penfigoampolla']['area_id'])) {
+      $encuentra = $this->Penfigoampolla->find('first', array(
         'recursive' => -1,
-        'conditions' => array('area_id' => $this->request->data['Penfigoampolla']['area_id'],'penfigo_id' => $idPenfigo)
+        'conditions' => array(
+          'area_id' => $this->request->data['Penfigoampolla']['area_id'],
+          'penfigo_id' => $idPenfigo,
+          'tipoampolla_id' => $this->request->data['Penfigoampolla']['tipoampolla_id']
+        )
       ));
-      if(!empty($encuentra)){
+      if (!empty($encuentra)) {
         $this->request->data['Penfigoampolla']['id'] = $encuentra['Penfigoampolla']['id'];
       }
       $this->Penfigoampolla->create();
@@ -31,4 +38,9 @@ class PenfigoampollasController extends AppController {
     }
     exit;
   }
+  public function elimina($idPenfigo = null,$idpampolla = null){
+    $this->Penfigoampolla->delete($idpampolla);
+    $this->redirect(array('action' => 'penfigoampolla',$idPenfigo));
+  }
+
 }
